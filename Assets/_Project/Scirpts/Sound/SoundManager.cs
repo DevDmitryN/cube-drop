a91ef149dc663cd87b3fca4a81b720bc5098d43a
DevDmitryN <dmitry.a.narkevich@gmail.com>
2024-01-21 17:18:18 +0300

    refactor(SoundManager): strike sounds as serialize field

--- Assets/_Project/Scirpts/Sound/SoundManager.cs
+++ Assets/_Project/Scirpts/Sound/SoundManager.cs
@@ -6,46 +6,35 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _coinSoundObjects;
-
-    private List<AudioSource> _coinAudioSoruces;
     
-    private const float STRIKE_SOUND_VOLUME = 0.5f;
-
-    private AudioSource _coinSource;
-
-    private List<AudioClip> _strikes = new List<AudioClip>();
-
-    private AudioSource _audioSource;
+    [SerializeField] private List<GameObject> _strikeSoundObjects;
+    
+    private List<AudioSource> _coinAudioSources;
+    
+    private List<AudioSource> _strikeAudioSources;
 
     private void Awake()
     {
-        _coinAudioSoruces = _coinSoundObjects.Select(x => x.GetComponent<AudioSource>()).ToList();
-        
-        _audioSource = GetComponent<AudioSource>();
-
-        for (int i = 1; i <= 10; i++)
-        {
-            _strikes.Add(Resources.Load<AudioClip>($"sounds/strikes/strike {i}"));
-        }
-
+        _coinAudioSources = _coinSoundObjects.Select(x => x.GetComponent<AudioSource>()).ToList();
+        _strikeAudioSources = _strikeSoundObjects.Select(x => x.GetComponent<AudioSource>()).ToList();
     }
 
     public void Strike()
     {
-        var random = new System.Random().Next(0, 10);
-        _audioSource.clip = _strikes[random];
-        _audioSource.volume = STRIKE_SOUND_VOLUME;
-        _audioSource.Play();
-        //_audioSource.PlayOneShot(_strikes[random]);
+        var randomIndex = new System.Random().Next(0, 6);
+        var audioSource = _strikeAudioSources[randomIndex];
+        _strikeAudioSources.RemoveAt(randomIndex);
+        _strikeAudioSources.Add(audioSource);
+        audioSource.Play();
     }
 
     public void Coin()
     {
         //_coinSource.Play();
 
-        var audioSource = _coinAudioSoruces[0];
-        _coinAudioSoruces.RemoveAt(0);
-        _coinAudioSoruces.Add(audioSource);
+        var audioSource = _coinAudioSources[0];
+        _coinAudioSources.RemoveAt(0);
+        _coinAudioSources.Add(audioSource);
         audioSource.Play();
     }
 }
